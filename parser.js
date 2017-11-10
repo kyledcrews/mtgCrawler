1d614508b592ea232443a2e06c2ce25dbccd8516
Waiting7777 <waiting0819@msn.com>
2017-11-10 17:24:33 +0800

    add vision part

--- parser.js
+++ parser.js
@@ -1,11 +1,63 @@
 const fs = require('fs')
 const Xray = require('x-ray')
 const x = Xray()
+const Vision = require('@google-cloud/vision')
+
+const vision = new Vision()
+
+let mapping = {}
+
 
 fs.readFile('./data/xln/M1.html', (err, data) => {
     if(err) throw err
-
+    
     x(data.toString(), 'style')(function(err, data){
+        var a = data.replace(/[\n\t\r]/g, '')
         console.log(data)
+        console.log(a.split('.'))
+        var b = a.split('.')
+        var re = /(-)?\d+/g
+
+        var filename = './price_icons.png'
+        
+        var request = {
+            source: {
+                filename: filename
+            }
+        }
+        
+        for(var i = 6; i < b.length; i++){
+            console.log(b[i], b[i].indexOf(' '))
+            console.log(b[i].substr(b[i].indexOf(' '), b[i].length).match(re))
+        }
+        
+    })
+
+    x(data.toString(), ['.deckdbbody_row .search_results_7'])(function(err, data){
+        // console.log(data)
+        var marker = [];
+        for(var i in data){
+            if(data[i] == 'NM/M') {
+                // console.log(i)
+                marker.push(i)
+            }
+        }
+        fs.readFile('./data/xln/M1.html', (err, data) => {
+            if(err) throw err
+
+            x(data.toString(), ['.deckdbbody_row .search_results_9@html'])(function(err, data){
+                
+                for(var i in data){
+                    if(marker.indexOf(i) != -1){
+                        // console.log(data[i])
+                        x(data[i], ['div@class'])(function(err, data){
+                            // console.log(data)
+                        })
+                    }
+                }    
+            })
+        })
+        
+        
     })
 })
